fde4befe12ffd3010e3934c077f9a10de6d5c4f3
manbucy <1412039603@qq.com>
2017-08-03 10:23:06 +0800

    初步实现 改变文件的权限，与恢复文件的权限

---
+++ app/src/test/java/net/manbucy/lockwallpaper/util/PermissionUtilsTest.java
@@ -0,0 +1,27 @@
+package net.manbucy.lockwallpaper.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * PermissionUtilsTest
+ * Created by yang on 2017/8/3.
+ */
+public class PermissionUtilsTest {
+    @Test
+    public void borrowPermission() throws Exception {
+        PermissionUtils.borrowPermission();
+    }
+
+    @Test
+    public void repayPermission() throws Exception {
+        PermissionUtils.repayPermission();
+    }
+
+    @Test
+    public void getPermission() throws Exception {
+        PermissionUtils.getPermission();
+    }
+
+}
